b0b5e1c6b35ffd8bf789e74bb7be76747c37f7ec
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-09-09 21:08:00 +0300

    Arac Entity Sinifi Olusturuldu

--- TransportationEntity/Nakliye.cs
+++ TransportationEntity/Nakliye.cs
@@ -8,9 +8,18 @@ namespace TransportationEntity
         [Required]
         public string TalepTipi { get; set; }
 
+        public bool NakliyeYapildimi { get; set; }
+
+        public string MusteriDegerlendirmeleri { get; set; }
+
         public string Aciklama { get; set; }
 
         public DateTime TalepTarihi { get; set; }
+
+        public int RezervasyonId { get; set; }
+        public int AracId { get; set; }
+        public Rezervasyon Rezervasyon { get; set; }
+        public List<Arac> Araclar { get; set; }
 
     }
 }
---
+++ TransportationEntity/Rezervasyon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportationEntity
+{
+    public class Rezervasyon
+    {
+        public int RezervasyonId { get; set; }
+        public int NakliyatId { get; set; }
+
+        public DateTime RezervasyonTarihi { get; set; }
+
+        public string NakliyeTipi { get; set; }// Evden eve tasima,Ofis Tasima,buyuk hacim ve agirlikta esya tasima
+
+        public Nakliye Nakliye { get; set; }
+
+    }
+}
